b69164540d8a6da8088b05d96c3fc73e743c5b59
Jschles1 <johnschlesinger5@gmail.com>
2022-08-15 19:11:10 -0400

    Add background animation

---
+++ src/components/Layout.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+interface Props {
+    children?: React.ReactNode;
+}
+
+const Waves: React.FC = () => (
+    <div className="absolute bottom-0 w-full">
+        <svg
+            className="relative w-full mb-[-7px] max-h-[10vh]"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shapeRendering="auto"
+        >
+            <defs>
+                <path id="wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g className="[&>*]:animate-waves">
+                <use
+                    className="!animation-delay-1000 !animation-duration-1000"
+                    xlinkHref="#wave"
+                    x="48"
+                    y="0"
+                    fill="rgba(255,255,255,0.7)"
+                />
+                <use
+                    className="!animation-delay-2000 !animation-duration-2000"
+                    xlinkHref="#wave"
+                    x="48"
+                    y="3"
+                    fill="rgba(255,255,255,0.5)"
+                />
+                <use
+                    className="!animation-delay-3000 !animation-duration-3000"
+                    xlinkHref="#gentle-wave"
+                    x="48"
+                    y="5"
+                    fill="rgba(255,255,255,0.3)"
+                />
+                <use
+                    className="!animation-delay-4000 !animation-duration-4000"
+                    xlinkHref="#gentle-wave"
+                    x="48"
+                    y="7"
+                    fill="#fff"
+                />
+            </g>
+        </svg>
+    </div>
+);
+
+const Layout: React.FC<Props> = ({ children }) => {
+    return (
+        <div className="bg-gradient-60 from-[#48c6ef] to-[blue] min-h-screen">
+            <div>{children}</div>
+            <Waves />
+        </div>
+    );
+};
+
+export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,65 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                waves: 'waves 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite',
+            },
+            keyframes: {
+                waves: {
+                    '0%': {
+                        transform: 'translate3d(-90px, 0, 0)',
+                    },
+                    '100%': {
+                        transform: 'translate3d(85px, 0, 0)',
+                    },
+                },
+            },
+            animationDelay: {
+                1000: '-2s',
+                2000: '-3s',
+                3000: '-4s',
+                4000: '-5s',
+            },
+            animationDuration: {
+                1000: '7s',
+                2000: '10s',
+                3000: '13s',
+                4000: '20s',
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        require('tailwindcss-animation'),
+        plugin(function ({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    'bg-gradient': (angle) => ({
+                        'background-image': `linear-gradient(${angle}, var(--tw-gradient-stops))`,
+                    }),
+                },
+                {
+                    // values from config and defaults you wish to use most
+                    values: Object.assign(
+                        theme('bgGradientDeg', {}), // name of config key. Must be unique
+                        {
+                            10: '10deg', // bg-gradient-10
+                            15: '15deg',
+                            20: '20deg',
+                            25: '25deg',
+                            30: '30deg',
+                            45: '45deg',
+                            60: '60deg',
+                            90: '90deg',
+                            120: '120deg',
+                            135: '135deg',
+                        }
+                    ),
+                }
+            );
+        }),
+    ],
 };
