1b21aff9e7e7174a2a3d4f5eb8f52f9dca83722c
Jschles1 <johnschlesinger5@gmail.com>
2022-08-18 18:21:01 -0400

    Update links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,27 @@
 import * as React from 'react';
 import NextLink from 'next/link';
-import { useRouter } from 'next/router';
 
 interface LinkProps {
     href: string;
     name: string;
-    activeHref: string;
 }
 
-const Link: React.FC<LinkProps> = ({ href, name, activeHref }) => {
-    const textStyle = href === activeHref ? 'font-bold border-b border-white' : 'font-normal';
+const Link: React.FC<LinkProps> = ({ href, name }) => {
     return (
         <NextLink href={href}>
-            <a className={textStyle}>{name}</a>
+            <a className="font-normal border-b border-transparent hover:font-bold hover:border-white">{name}</a>
         </NextLink>
     );
 };
 
 const Navigation: React.FC = () => {
-    const router = useRouter();
-    const activeHref = router.pathname;
     return (
         <div className="mt-6">
             <hr />
             <div className="flex mx-auto w-full gap-2 my-6 justify-around">
-                <Link href="/" name="About" activeHref={activeHref} />
-                <Link href="/education" name="Education" activeHref={activeHref} />
-                <Link href="/experience" name="Work Experience" activeHref={activeHref} />
-                <Link href="/projects" name="Projects" activeHref={activeHref} />
-                <Link href="/contact" name="Contact" activeHref={activeHref} />
+                <Link href="https://www.linkedin.com/in/john-schlesinger-21438362/" name="LinkedIn" />
+                <Link href="https://github.com/Jschles1" name="GitHub" />
+                <Link href="#" name="Download Resume" />
             </div>
             <hr />
         </div>
