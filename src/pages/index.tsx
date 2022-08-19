57cf80418fb68c10564999c208ce6f909ba361ad
Jschles1 <johnschlesinger5@gmail.com>
2022-08-18 20:35:10 -0400

    Add programming experience data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,23 @@ const skills = [
     'Expertise in implementing scalable CSS designs using SCSS and libraries such as MaterialUI and TailwindCSS.',
 ];
 
+const programmingExperience = [
+    {
+        role: 'Senior Frontend Developer',
+        company: 'Encora',
+        companyLogo: '/images/encora.svg',
+        startDate: 'January 2018',
+        endDate: 'Present',
+        description: '',
+        accomplishments: [
+            "Led frontend team for the marketing section of 1800Flower's PWA that was featured at Google I/O 2019 for being the first PWA app to be featured in Google's play store via Trusted Web Activities.",
+            "Worked closely with client design teams to implement new design on LG's ecommerce site, resulting in higher conversions due to a better user experience.",
+            "Led a team of three mid-level engineers to integrate Avon's frontend checkout flow with Cybersource's payment processor, allowing Avon to meet PCI security standards for their website.",
+            'Served as frontend lead on project to create an LG ecommerce store exclusively for LG employees and LG partners.',
+        ],
+    },
+];
+
 const Home: NextPage = () => {
     return (
         <>
