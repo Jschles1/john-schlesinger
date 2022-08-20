c83b8e8670f17e8e049a6f14cd01031574483a7d
Jschles1 <johnschlesinger5@gmail.com>
2022-08-20 17:26:02 -0400

    useMediaQuery and other mobile changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
 import * as React from 'react';
 import NextImage from 'next/image';
+import useMediaQuery from '../lib/useMediaQuery';
 
-const Header: React.FC = () => (
-    <div className="flex items-start mx-auto max-w-full gap-24 justify-center">
-        <div>
-            <NextImage className="rounded-full" src="/images/headshot2022.jpg" width={180} height={180} />
-        </div>
-        <div className="h-full relative">
-            <h1 className="text-4xl font-bold my-6">John Schlesinger</h1>
-            <div className="[&>p]:mb-1">
-                <p>&#128187; Senior Frontend Engineer</p>
-                <p>&#127947; Former Certified Personal Trainer</p>
-                <p className="font-bold">&#128231; johnschlesinger5@gmail.com</p>
+const Header: React.FC = () => {
+    const isMobile = useMediaQuery('(max-width: 769px)');
+    let imageDimensions = isMobile
+        ? {
+              width: 100,
+              height: 100,
+          }
+        : {
+              width: 180,
+              height: 180,
+          };
+    return (
+        <div className="block text-center xs:text-left xs:flex items-start mx-auto max-w-full gap-24 justify-center">
+            <div>
+                <NextImage
+                    className="rounded-full"
+                    src="/images/headshot2022.jpg"
+                    width={imageDimensions.width}
+                    height={imageDimensions.height}
+                />
+            </div>
+            <div className="h-full relative">
+                <h1 className="text-xl xs:text-4xl font-bold my-3 xs:my-6">John Schlesinger</h1>
+                <div className="[&>p]:mb-1">
+                    <p>&#128187; Senior Frontend Engineer</p>
+                    <p>&#127947; Former Certified Personal Trainer</p>
+                    <p className="font-bold">&#128231; johnschlesinger5@gmail.com</p>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Header;
