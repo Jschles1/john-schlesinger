4158abd92ffc0e73c263738ed79a25dfff1e444b
Jschles1 <johnschlesinger5@gmail.com>
2023-02-09 15:33:47 -0500

    Mobile adjustments

--- src/components/EducationItem.tsx
+++ src/components/EducationItem.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const EducationItem: React.FC<Props> = ({ name, date, description, location }) => (
     <li className="my-3">
-        <div className="flex items-center justify-between">
+        <div className="flex xs:items-center xs:justify-between flex-col xs:flex-row">
             <p className="font-bold">{name}</p>
             <p>{location}</p>
         </div>
 
-        <div className="flex items-center justify-between">
+        <div className="flex xs:items-center xs:justify-between flex-col xs:flex-row">
             <p className="italic">{description}</p>
             <p>{date}</p>
         </div>
